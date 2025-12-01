d1b8aa9c77efdf290d6566b86b43326d0a87c93e
Arseni <esenin.magmel@gmail.com>
2025-12-01 21:28:37 +0300

    create simple wifi connect

--- app/src/main/java/by/arsy/wificonnector/MainActivity.kt
+++ app/src/main/java/by/arsy/wificonnector/MainActivity.kt
@@ -1,14 +1,19 @@
 package by.arsy.wificonnector
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -17,12 +22,18 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.core.content.ContextCompat
 import by.arsy.wificonnector.ui.theme.WiFiConnectorTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        contract = ActivityResultContracts.RequestMultiplePermissions()
+    ) {}
     private val mainViewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        checkAndRequestPermissions()
         enableEdgeToEdge()
         setContent {
             WiFiConnectorTheme {
@@ -31,6 +42,8 @@ class MainActivity : ComponentActivity() {
                     Greeting(
                         text = text,
                         onTextChange = mainViewModel::updateText,
+                        onCreateWifi = mainViewModel::createEndpoint,
+                        onAskWifi = mainViewModel::discoveryEndpoint,
                         modifier = Modifier
                             .fillMaxSize()
                             .padding(innerPadding)
@@ -39,12 +52,42 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    private fun checkAndRequestPermissions() {
+        val permissionsToRequest = mutableListOf(
+            Manifest.permission.NEARBY_WIFI_DEVICES,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        )
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            permissionsToRequest.addAll(
+                listOf(
+                    Manifest.permission.BLUETOOTH_SCAN,
+                    Manifest.permission.BLUETOOTH_ADVERTISE,
+                    Manifest.permission.BLUETOOTH_CONNECT
+                )
+            )
+        }
+
+        val notGranted = permissionsToRequest.filter {
+            ContextCompat.checkSelfPermission(
+                this,
+                it
+            ) != PackageManager.PERMISSION_GRANTED
+        }
+
+        if (notGranted.isNotEmpty()) {
+            requestPermissionLauncher.launch(input = notGranted.toTypedArray())
+        }
+    }
 }
 
 @Composable
 fun Greeting(
     text: String,
     onTextChange: (String) -> Unit,
+    onCreateWifi: () -> Unit,
+    onAskWifi: () -> Unit,
     modifier: Modifier = Modifier
 ) {
     Column(
@@ -52,6 +95,14 @@ fun Greeting(
         verticalArrangement = Arrangement.Center,
         modifier = modifier
     ) {
+        Button(onClick = onCreateWifi) {
+            Text(text = "Create endpoint")
+        }
+
+        Button(onClick = onAskWifi) {
+            Text(text = "Discovery endpoint")
+        }
+
         TextField(
             value = text,
             onValueChange = { onTextChange(it) }
--- app/src/main/java/by/arsy/wificonnector/MainViewModel.kt
+++ app/src/main/java/by/arsy/wificonnector/MainViewModel.kt
@@ -1,15 +1,87 @@
 package by.arsy.wificonnector
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
+import com.google.android.gms.nearby.Nearby
+import com.google.android.gms.nearby.connection.AdvertisingOptions
+import com.google.android.gms.nearby.connection.ConnectionInfo
+import com.google.android.gms.nearby.connection.ConnectionLifecycleCallback
+import com.google.android.gms.nearby.connection.ConnectionResolution
+import com.google.android.gms.nearby.connection.DiscoveredEndpointInfo
+import com.google.android.gms.nearby.connection.DiscoveryOptions
+import com.google.android.gms.nearby.connection.EndpointDiscoveryCallback
+import com.google.android.gms.nearby.connection.Payload
+import com.google.android.gms.nearby.connection.PayloadCallback
+import com.google.android.gms.nearby.connection.PayloadTransferUpdate
+import com.google.android.gms.nearby.connection.Strategy
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
+
+private const val SERVICE_ID = "by.arsy.wificonnector"
 class MainViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val connectionsClient = Nearby.getConnectionsClient(application)
+    private var connectEndpointId = ""
     private val _text = MutableStateFlow("")
     val text = _text.asStateFlow()
 
+    private val payloadCallback = object : PayloadCallback() {
+        override fun onPayloadReceived(endpointId: String, payload: Payload) {
+            if (payload.type == Payload.Type.BYTES) {
+                _text.value = String(bytes = payload.asBytes()!!)
+            }
+        }
+
+        override fun onPayloadTransferUpdate(p0: String, p1: PayloadTransferUpdate) {}
+    }
+
+    private val connectionCallback = object : ConnectionLifecycleCallback() {
+        override fun onConnectionInitiated(endpointId: String, connectionInfo: ConnectionInfo) {
+            connectEndpointId = endpointId
+            connectionsClient.acceptConnection(endpointId, payloadCallback)
+        }
+
+        override fun onConnectionResult(p0: String, p1: ConnectionResolution) {}
+        override fun onDisconnected(p0: String) {}
+    }
+
+    private val endpointDiscoveryCallback = object : EndpointDiscoveryCallback() {
+        override fun onEndpointFound(
+            endpointId: String,
+            discoveredEndpointInfo: DiscoveredEndpointInfo
+        ) {
+            connectionsClient.requestConnection("Multiplayer", endpointId, connectionCallback)
+        }
+
+        override fun onEndpointLost(endpointId: String) {}
+    }
+
+    fun createEndpoint() {
+        val options = AdvertisingOptions.Builder().setStrategy(Strategy.P2P_STAR).build()
+        connectionsClient.startAdvertising(
+            "WiFiConnector",
+            SERVICE_ID,
+            connectionCallback,
+            options
+        )
+    }
+
+    fun discoveryEndpoint() {
+        val options = DiscoveryOptions.Builder().setStrategy(Strategy.P2P_STAR).build()
+        connectionsClient.startDiscovery(SERVICE_ID, endpointDiscoveryCallback, options)
+            .addOnSuccessListener { Log.d("discoveryEndpoint", "Success") }
+            .addOnFailureListener { e -> Log.e("discoveryEndpoint", "Failure", e) }
+    }
+
+    fun sendText(text: String, toEndpointId: String) {
+        val bytesPayload = Payload.fromBytes(text.toByteArray(Charsets.UTF_8))
+        connectionsClient.sendPayload(toEndpointId, bytesPayload)
+    }
+
     fun updateText(text: String) {
+        sendText(text, connectEndpointId)
         _text.value = text
     }
 }
